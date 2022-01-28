1f0c06e95834b9f9c8a211853004040978046273
David Alexander Gonzalez Cespedes <david.gc@globant.com>
2022-01-28 12:37:41 -0500

    Initializing UniversityClasses

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import people.FullTimeTeacher;
 import people.PartTimeTeacher;
 import people.Student;
+import people.Teacher;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
 
@@ -13,6 +16,55 @@ public class Main {
 
         initializeTeachers();
         initializeStudents();
+        initializeUniversityClasses();
+    }
+
+    public static void initializeUniversityClasses(){
+
+        List<Student> studentsClass1 = new LinkedList<>();
+        studentsClass1.add(university.findStudentById(123));
+        studentsClass1.add(university.findStudentById(234));
+        studentsClass1.add(university.findStudentById(345));
+
+        Teacher teacherClass1= university.findTeacherById(2123);
+
+        UniversityClass universityClass1= new UniversityClass(1, "Calculus I", "Room 1", studentsClass1, teacherClass1);
+
+        List<Student> studentsClass2 = new LinkedList<>();
+        studentsClass2.add(university.findStudentById(234));
+        studentsClass2.add(university.findStudentById(456));
+        studentsClass2.add(university.findStudentById(567));
+        studentsClass2.add(university.findStudentById(123));
+
+        Teacher teacherClass2= university.findTeacherById(2234);
+
+        UniversityClass universityClass2= new UniversityClass(2, "OOP", "Room 1", studentsClass2, teacherClass2);
+
+        List<Student> studentsClass3 = new LinkedList<>();
+        studentsClass3.add(university.findStudentById(567));
+        studentsClass3.add(university.findStudentById(678));
+
+        Teacher teacherClass3= university.findTeacherById(3123);
+
+        UniversityClass universityClass3= new UniversityClass(3, "Databases", "Room 2", studentsClass3, teacherClass3);
+
+        List<Student> studentsClass4 = new LinkedList<>();
+        studentsClass4.add(university.findStudentById(123));
+        studentsClass4.add(university.findStudentById(234));
+        studentsClass4.add(university.findStudentById(345));
+        studentsClass4.add(university.findStudentById(456));
+        studentsClass4.add(university.findStudentById(567));
+        studentsClass4.add(university.findStudentById(678));
+
+        Teacher teacherClass4= university.findTeacherById(3234);
+
+        UniversityClass universityClass4= new UniversityClass(4, "Data Structures", "Room 3", studentsClass4, teacherClass4);
+
+
+        university.addUniversityClass(universityClass1);
+        university.addUniversityClass(universityClass2);
+        university.addUniversityClass(universityClass3);
+        university.addUniversityClass(universityClass4);
     }
 
     public static void initializeTeachers() {
