3495c2980b3cf30d3d545737498af519e83744a8
David Alexander Gonzalez Cespedes <david.gc@globant.com>
2022-01-28 11:04:13 -0500

    Created FullTimeTeacher class

---
+++ src/people/FullTimeTeacher.java
@@ -0,0 +1,16 @@
+package people;
+
+public class FullTimeTeacher extends Teacher {
+
+    private int yearsOfExperience;
+
+    public FullTimeTeacher(String name, double baseSalary, int yearsOfExperience) {
+        super(name, baseSalary);
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+    @Override
+    public double getSalary() {
+        return getBaseSalary()*yearsOfExperience*1.1;
+    }
+}
