9e3f06053a17a20d37fbe55346a60950ce983713
David Alexander Gonzalez Cespedes <david.gc@globant.com>
2022-01-28 12:24:26 -0500

    Finding teachers and students by id, universityClasses by code

--- src/University.java
+++ src/University.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 
 public class University {
 
-    private HashMap<Long,Teacher> teachers;
+    private HashMap<Long, Teacher> teachers;
     private HashMap<Long, Student> students;
     private HashMap<Long, UniversityClass> universityClasses;
 
@@ -15,18 +15,30 @@ public class University {
         universityClasses = new HashMap<>();
     }
 
-    public University(HashMap<Long,Teacher> teachers, HashMap<Long,Student> students, HashMap<Long,UniversityClass> universityClasses) {
+    public University(HashMap<Long, Teacher> teachers, HashMap<Long, Student> students, HashMap<Long, UniversityClass> universityClasses) {
         this.teachers = teachers;
         this.students = students;
         this.universityClasses = universityClasses;
     }
 
+    public Teacher findTeacherById(long id) {
+        return teachers.get(id);
+    }
+
+    public Student findStudentById(long id){
+        return students.get(id);
+    }
+
+    public UniversityClass findUniversityClassByCode(long code){
+        return universityClasses.get(code);
+    }
+
     public void addTeacher(Teacher teacher) {
-        teachers.put(teacher.getId(),teacher);
+        teachers.put(teacher.getId(), teacher);
     }
 
     public void addStudent(Student student) {
-        students.put(student.getId(),student);
+        students.put(student.getId(), student);
     }
 
     public void addUniversityClass(UniversityClass universityClass) {
@@ -37,11 +49,11 @@ public class University {
         return teachers;
     }
 
-    public void setTeachers(HashMap<Long, Teacher>  teachers) {
+    public void setTeachers(HashMap<Long, Teacher> teachers) {
         this.teachers = teachers;
     }
 
-    public HashMap<Long, Student>  getStudents() {
+    public HashMap<Long, Student> getStudents() {
         return students;
     }
 
@@ -49,11 +61,11 @@ public class University {
         this.students = students;
     }
 
-    public HashMap<Long, UniversityClass>  getUniversityClasses() {
+    public HashMap<Long, UniversityClass> getUniversityClasses() {
         return universityClasses;
     }
 
-    public void setUniversityClasses(HashMap<Long, UniversityClass>  universityClasses) {
+    public void setUniversityClasses(HashMap<Long, UniversityClass> universityClasses) {
         this.universityClasses = universityClasses;
     }
 }
