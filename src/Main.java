cd06d5b972df0687f168549fc2b02b60f4f8f13a
David Alexander Gonzalez Cespedes <david.gc@globant.com>
2022-01-28 11:34:08 -0500

    Calling initializeTeachers method

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ public class Main {
 
     public static void main(String[] args) {
         university = new University();
+
+        initializeTeachers();
     }
 
     public static void initializeTeachers() {
