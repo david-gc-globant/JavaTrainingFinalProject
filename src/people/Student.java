67f4472c6dba393d2a9340902f18d1bbdae7ae2d
David Alexander Gonzalez Cespedes <david.gc@globant.com>
2022-01-28 09:48:24 -0500

    Created Student class

---
+++ src/people/Student.java
@@ -0,0 +1,28 @@
+package people;
+
+import java.util.Calendar;
+
+public class Student extends Person {
+
+    private long id;
+    private Calendar dateOfBirth;
+
+    public Student(String name, long id, Calendar dateOfBirth) {
+        super(name);
+        this.id = id;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getAge() {
+
+        Calendar today = Calendar.getInstance();
+
+        int age = today.get(Calendar.YEAR) - dateOfBirth.get(Calendar.YEAR);
+
+        if (today.get(Calendar.DAY_OF_YEAR) < dateOfBirth.get(Calendar.DAY_OF_YEAR)) {
+            age--;
+        }
+
+        return age;
+    }
+}
